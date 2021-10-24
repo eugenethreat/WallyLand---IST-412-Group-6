9a10aaf20914b926988398a7fbb6c336163d9b5b
eugenethreat <eugryooene@gmail.com>
2021-10-24 12:17:18 -0400

    testing eugene-edit

--- src/WLVG_app/testharness/testharness.java
+++ src/WLVG_app/testharness/testharness.java
@@ -57,6 +57,8 @@ public class testharness {
         testFinalizedSale();
         testPaymentCntl();
         testPaymentList();
+        
+        System.out.println("testing...");
 
         // ************************************
         // *****   package OrderFood  ******
